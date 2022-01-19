c2228d39d5d72b880d1ace52e9d5a8a30f4b2ceb
pradnyeshgaonkar123 <pradnyeshgaonkar106@gmail.com>
2022-01-19 12:49:46 +0530

    added poc2 rolebased inmemory

---
+++ src/main/java/com/studentProject/security/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.studentProject.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {		
+		Optional<User> user = userRepository.findByUserName(userName);
+		return user.map(MyUserDetails::new).get();
+	}
+	
+}
